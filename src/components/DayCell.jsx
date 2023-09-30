fc58fbd622ddbe3a82e2bb4c3898a6505b4d24f0
Martin <97972556+VgHost-26@users.noreply.github.com>
2023-09-30 20:09:04 +0200

    wrong months fixed

--- src/components/DayCell.jsx
+++ src/components/DayCell.jsx
@@ -7,7 +7,7 @@ export function DayCell({
   movies,
   images,
 }) {
-  let sMonth = parseInt(month) <= 9 ? "0" + (parseInt(month) + 1) : month
+  let sMonth = parseInt(month) <= 8 ? ("0" + (parseInt(month) + 1)) : (parseInt(month) + 1)
   let sDay = parseInt(day) <= 9 ? "0" + day : day
   let date = year + "-" + sMonth + "-" + sDay
 
@@ -30,7 +30,7 @@ export function DayCell({
       <div className="day-number">{day}</div>
 
       {movies.map(movie => {
-        if (movie.date == date) {
+        if (movie.date === date) {
           return (
             <>
               <div key={movie.id} className="movie-title">
